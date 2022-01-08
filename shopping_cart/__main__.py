cbadcee645e2ce81d0266f4b438361aa62a29593
Benjamin Anderson <benjaminanderson98@live.com>
2022-01-08 15:06:24 -0700

    update

--- shopping_cart/__main__.py
+++ shopping_cart/__main__.py
@@ -1,18 +1,6 @@
-from tkinter import *
-from tkinter.ttk import *
-
 
 def main():
-    root = Tk()
-    root.title("First Tkinter Program")
-
-    frame = Frame(root)
-    frame.pack()
-
-    button = Button(frame, text = "Bro")
-    button.pack()
-
-    root.mainloop()
+    pass
 
 if __name__ == "__main__":
     main()
